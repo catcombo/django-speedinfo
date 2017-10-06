d0a444184e8e043f7fc0197e006ed544f8c34b4d
Evgeniy Krysanov <evgeniy.krysanov@gmail.com>
2017-10-06 16:31:40 +0300

    Add package setup files.

---
+++ setup.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+from os.path import join, dirname
+from setuptools import setup
+
+
+with open(join(dirname(__file__), 'README.rst')) as f:
+    long_description = f.read()
+
+setup(
+    name='django-speedinfo',
+    version='1.0.0',
+    packages=['speedinfo'],
+    include_package_data=True,
+    install_requires=['Django>=1.10'],
+    license='MIT',
+    description='Live profiling tool for Django framework to measure views performance',
+    long_description=long_description,
+    url='https://github.com/catcombo/django-speedinfo',
+    author='Evgeniy Krysanov',
+    author_email='evgeniy.krysanov@gmail.com',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.10',
+        'Framework :: Django :: 1.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    keywords='django profiler views performance',
+)
