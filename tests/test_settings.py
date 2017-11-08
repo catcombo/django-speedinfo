57bdcedb630e928798d1dbb46881cd14c05ce999
Evgeniy Krysanov <evgeniy.krysanov@gmail.com>
2017-11-08 17:16:33 +0300

    Add integration with Travis CI.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -50,8 +50,11 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
     }
 }
 
