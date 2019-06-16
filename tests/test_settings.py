2801381cdcc78caa5ad4a3f2b0362bd836491697
Evgeniy Krysanov <evgeniy.krysanov@gmail.com>
2019-06-16 17:47:00 +0300

    Update test settings

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -6,8 +6,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'secret-key'
+SECRET_KEY = 'dummy'
 DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
