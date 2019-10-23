09f4d1d56cbe42a1c6d08fb4704ae4da582e792b
Evgeniy Krysanov <evgeniy.krysanov@gmail.com>
2019-10-23 11:00:51 +0300

    Override `count()` method in ViewProfilerQuerySet

--- speedinfo/managers.py
+++ speedinfo/managers.py
@@ -8,9 +8,12 @@ from speedinfo import profiler
 class ViewProfilerQuerySet(models.QuerySet):
     """
     Overrides standard QuerySet behaviour to return objects
-    from profiler storage. Hack is working only for `all()` and
-    `order_by()` methods and it was made for integration with
-    Django admin.
+    from profiler storage. Hack is working only for `all()`,
+    `order_by()` and `count()` methods and was made for integration
+    with Django admin.
     """
     def _fetch_all(self):
         self._result_cache = profiler.storage.fetch_all(self.query.order_by)
+
+    def count(self):
+        return len(self)
